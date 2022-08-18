f7df1033855a779e831c582a059b4f9588931433
Thais Paiva <hello@thaispaiva.info>
2022-08-18 22:12:39 +0200

    basic user journey implemented

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,34 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to @bookmark.list
+  end
+
+  private
+
+  def bookmark_params
+    # coisas que vêm nno params (@list_id já tá aqui)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,4 +6,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index new create show] do
     resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: [:destroy]
 end
