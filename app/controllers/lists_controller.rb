4cdc6e86e3822bd6a43c1fb43cd2b82363eab89f
Thais Paiva <hello@thaispaiva.info>
2022-08-18 17:11:27 +0200

    list index and new

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# refactor: use the API to scrape from external database
+
+require 'faker'
+Movie.destroy_all
+
+20.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Hipster.sentence,
+    poster_url: 'https://picsum.photos/200/300',
+    rating: rand(1.0..10.0).round(1)
+  )
+end
